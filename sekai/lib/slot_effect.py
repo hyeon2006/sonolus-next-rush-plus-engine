43b570ef8315abf8caeb41a29d4b2ad6ae90e53b
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-10-25 20:07:40 +0900

    feat: v1 slot glow effect

--- sekai/lib/slot_effect.py
+++ sekai/lib/slot_effect.py
@@ -4,6 +4,7 @@ from sonolus.script.sprite import Sprite
 
 from sekai.lib.layer import LAYER_SLOT_EFFECT, LAYER_SLOT_GLOW_EFFECT, get_z
 from sekai.lib.layout import layout_slot_effect, layout_slot_glow_effect
+from sekai.lib.options import Options
 
 SLOT_GLOW_EFFECT_DURATION = 0.25
 SLOT_EFFECT_DURATION = 0.5
@@ -17,7 +18,7 @@ def draw_slot_glow_effect(
     size: float,
 ):
     progress = unlerp_clamped(start_time, end_time, time())
-    height = unlerp_clamped(1, 0.8, progress)
+    height = unlerp_clamped(1, 0.8, progress) if Options.version == 0 else 1 - lerp(1, 0, progress) ** 3
     layout = layout_slot_glow_effect(lane, size, height)
     z = get_z(LAYER_SLOT_GLOW_EFFECT, start_time, lane, invert_time=True)
     a = lerp(1, 0, progress)
