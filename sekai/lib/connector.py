bb90b7d44d7fe121db270cef35d9757ae5e95441
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-11-20 04:03:55 +0900

    fix: connector layer

--- sekai/lib/connector.py
+++ sekai/lib/connector.py
@@ -457,7 +457,7 @@ def draw_connector(
         if visual_state == ConnectorVisualState.ACTIVE and active_sprite.is_available:
             if Options.connector_animation:
                 a1, a2 = get_cross_fate_opacities(base_a, time() - segment_head_target_time, 0.5)
-                normal_sprite.draw(layout, z=z + 1 / 128, a=a1)
+                normal_sprite.draw(layout, z=z, a=a1)
                 active_sprite.draw(layout, z=z, a=a2)
             else:
                 normal_sprite.draw(layout, z=z, a=base_a)
