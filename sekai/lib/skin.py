414f224972a816257bfe3cf05c9291bf85cd8237
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-11-24 19:41:48 +0900

    feat: Add skin background

--- sekai/lib/skin.py
+++ sekai/lib/skin.py
@@ -13,6 +13,7 @@ from sekai.lib.options import Options
 @skin
 class Skin:
     cover: StandardSprite.STAGE_COVER
+    background: Sprite = sprite("Sekai Black Background")
 
     lane: StandardSprite.LANE
     judgment_line: StandardSprite.JUDGMENT_LINE
