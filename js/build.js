002ef2494baf6737877b1e86194287df82473048
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-12-08 13:11:05 +0900

    fix: root

--- js/build.js
+++ js/build.js
@@ -1,9 +1,9 @@
 import { copyFileSync } from 'node:fs'
 
-copyFileSync('../build/dist/engine/EngineConfiguration', '../dist/EngineConfiguration')
-copyFileSync('../build/dist/engine/EnginePlayData', '../dist/EnginePlayData')
-copyFileSync('../build/dist/engine/EngineWatchData', '../dist/EngineWatchData')
-copyFileSync('../build/dist/engine/EnginePreviewData', '../dist/EnginePreviewData')
-copyFileSync('../build/dist/engine/EngineTutorialData', '../dist/EngineTutorialData')
-copyFileSync('../build/dist/engine/EngineRom', '../dist/EngineRom')
-copyFileSync('./res/thumbnail.png', '../dist/thumbnail.png')
+copyFileSync('../build/dist/engine/EngineConfiguration', './dist/EngineConfiguration')
+copyFileSync('../build/dist/engine/EnginePlayData', './dist/EnginePlayData')
+copyFileSync('../build/dist/engine/EngineWatchData', './dist/EngineWatchData')
+copyFileSync('../build/dist/engine/EnginePreviewData', './dist/EnginePreviewData')
+copyFileSync('../build/dist/engine/EngineTutorialData', './dist/EngineTutorialData')
+copyFileSync('../build/dist/engine/EngineRom', './dist/EngineRom')
+copyFileSync('./res/thumbnail.png', './dist/thumbnail.png')
