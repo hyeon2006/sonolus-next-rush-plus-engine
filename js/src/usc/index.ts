380d592d7b75224b69110ba39bef7ad13de10c76
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-12-08 12:39:33 +0900

    feat: Update publish process

--- js/src/usc/index.ts
+++ js/src/usc/index.ts
@@ -3,20 +3,31 @@ export type USC = {
     objects: USCObject[]
 }
 
-export type USCObject = USCBpmChange | USCTimeScaleChange | USCSingleNote | USCSlideNote
+export type USCObject =
+    | USCBpmChange
+    | USCTimeScaleChange
+    | USCSingleNote
+    | USCSlideNote
+    | USCGuideNote
+    | USCDamageNote
+    | USCEvent
 
 type BaseUSCObject = {
     beat: number
+    timeScaleGroup: number
 }
 
-export type USCBpmChange = BaseUSCObject & {
+export type USCBpmChange = Omit<BaseUSCObject, 'timeScaleGroup'> & {
     type: 'bpm'
     bpm: number
 }
 
-export type USCTimeScaleChange = BaseUSCObject & {
-    type: 'timeScale'
-    timeScale: number
+export type USCTimeScaleChange = {
+    type: 'timeScaleGroup'
+    changes: {
+        beat: number
+        timeScale: number
+    }[]
 }
 
 type BaseUSCNote = BaseUSCObject & {
@@ -26,49 +37,80 @@ type BaseUSCNote = BaseUSCObject & {
 
 export type USCSingleNote = BaseUSCNote & {
     type: 'single'
-    trace: boolean
     critical: boolean
-    direction?: 'left' | 'up' | 'right'
+    trace: boolean
+    direction?: 'left' | 'up' | 'right' | 'none'
+}
+
+export type USCDamageNote = BaseUSCNote & {
+    type: 'damage'
 }
 
 export type USCConnectionStartNote = BaseUSCNote & {
     type: 'start'
-    trace: boolean
     critical: boolean
-    ease: 'out' | 'linear' | 'in'
-}
-
-export type USCConnectionIgnoreNote = BaseUSCNote & {
-    type: 'ignore'
-    ease: 'out' | 'linear' | 'in'
+    ease: 'out' | 'linear' | 'in' | 'inout' | 'outin'
+    judgeType: 'normal' | 'trace' | 'none'
 }
 
 export type USCConnectionTickNote = BaseUSCNote & {
     type: 'tick'
-    trace: boolean
-    critical: boolean
-    ease: 'out' | 'linear' | 'in'
+    critical?: boolean
+    ease: 'out' | 'linear' | 'in' | 'inout' | 'outin'
+    judgeType?: 'normal' | 'trace'
 }
 
-export type USCConnectionAttachNote = BaseUSCObject & {
+export type USCConnectionAttachNote = Omit<BaseUSCObject, 'timeScaleGroup'> & {
     type: 'attach'
-    critical: boolean
+    critical?: boolean
+    timeScaleGroup?: number
 }
 
 export type USCConnectionEndNote = BaseUSCNote & {
     type: 'end'
-    trace: boolean
     critical: boolean
     direction?: 'left' | 'up' | 'right'
+    judgeType: 'normal' | 'trace' | 'none'
 }
 
 export type USCSlideNote = {
-    type: 'slide'
-    active: boolean
+    type: 'slide' | 'guide'
     critical: boolean
     connections: [
-        USCConnectionStartNote | USCConnectionIgnoreNote,
-        ...(USCConnectionIgnoreNote | USCConnectionTickNote | USCConnectionAttachNote)[],
-        USCConnectionEndNote | USCConnectionIgnoreNote,
+        USCConnectionStartNote,
+        ...(USCConnectionTickNote | USCConnectionAttachNote)[],
+        USCConnectionEndNote,
     ]
+}
+export const USCColor = {
+    neutral: 0,
+    red: 1,
+    green: 2,
+    blue: 3,
+    yellow: 4,
+    purple: 5,
+    cyan: 6,
+    black: 7,
+}
+export type USCColor = keyof typeof USCColor
+
+export type USCGuideMidpointNote = BaseUSCNote & {
+    ease: 'out' | 'linear' | 'in' | 'inout' | 'outin'
+}
+
+export const USCFade = {
+    in: 2,
+    out: 0,
+    none: 1,
+}
+export type USCFade = keyof typeof USCFade
+export type USCGuideNote = {
+    type: 'guide'
+    color: USCColor
+    fade: USCFade
+    midpoints: USCGuideMidpointNote[]
+}
+export type USCEvent = {
+    type: 'skill' | 'feverChance' | 'feverStart'
+    beat: number
 }
