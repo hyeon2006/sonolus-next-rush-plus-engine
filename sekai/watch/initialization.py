a9ba95f68964e868aca274fc18acade5fd25625d
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2026-01-11 19:56:14 -0800

    Implement scoring modes

--- sekai/watch/initialization.py
+++ sekai/watch/initialization.py
@@ -1,9 +1,11 @@
-from sonolus.script.archetype import WatchArchetype, callback
+from sonolus.script.archetype import WatchArchetype, callback, imported
 
 from sekai.lib import archetype_names
 from sekai.lib.buckets import init_buckets
 from sekai.lib.layout import init_layout
+from sekai.lib.level_config import init_level_config
 from sekai.lib.note import init_note_life, init_score
+from sekai.lib.options import ConcreteScoreMode, ScoreMode
 from sekai.lib.particle import init_particles
 from sekai.lib.skin import init_skin
 from sekai.lib.stage import schedule_lane_sfx
@@ -16,14 +18,17 @@ from sekai.watch.stage import WatchScheduledLaneEffect, WatchStage
 class WatchInitialization(WatchArchetype):
     name = archetype_names.INITIALIZATION
 
+    score_mode: ConcreteScoreMode = imported(name="scoreMode", default=ScoreMode.UNWEIGHTED_COMBO)
+
     @callback(order=-1)
     def preprocess(self):
+        init_level_config(self.score_mode)
         init_layout()
         init_ui()
         init_skin()
         init_particles()
         init_buckets()
-        init_score()
+        init_score(WATCH_NOTE_ARCHETYPES)
 
         for note_archetype in WATCH_NOTE_ARCHETYPES:
             init_note_life(note_archetype)
