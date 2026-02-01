81d0ca06b8060c179114afd7e3b2259225470b46
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-02-02 02:19:54 +0900

    feat: added skill effect selection option

--- sekai/watch/initialization.py
+++ sekai/watch/initialization.py
@@ -15,7 +15,6 @@ from sonolus.script.runtime import is_replay, level_score
 
 from sekai.lib import archetype_names
 from sekai.lib.buckets import init_buckets
-from sekai.lib.events import SkillEffects
 from sekai.lib.layer import (
     LAYER_BACKGROUND_SIDE,
     LAYER_DAMAGE,
@@ -35,7 +34,7 @@ from sekai.lib.level_config import (
     init_ui_version,
 )
 from sekai.lib.note import init_life, init_score
-from sekai.lib.options import Options
+from sekai.lib.options import Options, SkillMode
 from sekai.lib.particle import ActiveParticles, init_particles
 from sekai.lib.skin import ActiveSkin, init_skin
 from sekai.lib.stage import schedule_lane_sfx
@@ -182,11 +181,9 @@ def setting_combo(head: int, skill: int) -> None:
 
     while ptr > 0:
         if skill_ptr > 0 and note.WatchBaseNote.at(ptr).target_time >= Skill.at(skill_ptr).start_time:
-            if Skill.at(skill_ptr).effect == SkillEffects.HEAL:
+            if Skill.at(skill_ptr).effect == SkillMode.HEAL:
                 skill_ptr = Skill.at(skill_ptr).next_ref.index
-            elif (
-                Skill.at(skill_ptr).effect == SkillEffects.SCORE or Skill.at(skill_ptr).effect == SkillEffects.JUDGMENT
-            ):
+            elif Skill.at(skill_ptr).effect == SkillMode.SCORE or Skill.at(skill_ptr).effect == SkillMode.JUDGMENT:
                 note.WatchBaseNote.at(ptr).entity_score_multiplier += (
                     note.WatchBaseNote.at(ptr).archetype_score_multiplier
                     + note.WatchBaseNote.at(ptr).entity_score_multiplier
@@ -400,7 +397,7 @@ def count_skill(head: int) -> None:
     while ptr > 0:
         Skill.at(ptr).count = count
         count += 1
-        if Skill.at(ptr).effect == SkillEffects.HEAL:
+        if Skill.at(ptr).effect == SkillMode.HEAL:
             life = clamp(life + 250, 0, 2000)
         Skill.at(ptr).current_life = life
         ptr = Skill.at(ptr).next_ref.index
