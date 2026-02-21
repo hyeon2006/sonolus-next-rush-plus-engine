45bddf85cb60805e780347c0f2af5fceced6feca
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-02-22 06:59:17 +0900

    fix: specify a range within the 2^31 limit

--- sekai/lib/layer.py
+++ sekai/lib/layer.py
@@ -49,6 +49,6 @@ def get_z(layer: int, time: float = 0.0, lane: float = 0.0, etc: int = 0, *, inv
         quantize_to_step(
             time - quantized_time if invert_time else quantized_time - time, start=-30, stop=30, step=1 / 256
         ),
-        quantize_to_step(abs(lane) + (1 / 20) * (lane > 0), start=0, stop=20, step=1 / 20),
+        quantize_to_step(abs(lane) + (1 / 16) * (lane > 0), start=0, stop=16, step=1 / 16),
         quantize_to_step(etc, start=0, stop=12, step=1),
     )
