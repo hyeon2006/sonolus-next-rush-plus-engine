68df310815731bec111034ec244c2c34d507ba48
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-03-28 14:58:34 +0900

    refactor: abstract critical flick particle logic and optimize cleanup

--- sekai/watch/particle_manager.py
+++ sekai/watch/particle_manager.py
@@ -1,31 +1,19 @@
 from __future__ import annotations
 
-from math import ceil, floor
-
 from sonolus.script.archetype import WatchArchetype, entity_memory
-from sonolus.script.array import Dim
-from sonolus.script.containers import ArrayMap, ArraySet
-from sonolus.script.globals import level_memory
 from sonolus.script.particle import ParticleHandle
 from sonolus.script.record import Record
 from sonolus.script.runtime import is_skip, time
 
 from sekai.lib import archetype_names
-from sekai.lib.layout import layout_lane
-from sekai.lib.options import Options
 from sekai.lib.particle import NoteParticleSet
+from sekai.lib.particle_manager import ParticleHandler, handle_critical_flick_lane_effect
 
 
 class ParticleEntry(Record):
     particle: ParticleHandle
     spawn_time: float
     chunk_id: float
-
-
-@level_memory
-class ParticleHandler:
-    critical_flick_lane_effect: ArrayMap[float, ParticleEntry, Dim[256]]
-    current_chunk_id: float
 
 
 class ParticleManager(WatchArchetype):
@@ -50,74 +38,11 @@ class ParticleManager(WatchArchetype):
         if self.check:
             return
 
-        handle_critical_flick_lane_effect(self.particles, self.lane, self.size, self.target_time)
+        is_time_skipped = abs(time() - self.target_time) > 1
+        handle_critical_flick_lane_effect(
+            self.particles, self.lane, self.size, self.target_time, force_clear=is_time_skipped
+        )
         self.check = True
 
     def terminate(self):
         self.check = False
-
-
-def handle_critical_flick_lane_effect(particles: NoteParticleSet, center_lane: float, size: float, spawn_time: float):
-    ParticleHandler.current_chunk_id = (ParticleHandler.current_chunk_id + 1) % 6
-    chunk_id = ParticleHandler.current_chunk_id
-
-    keys_to_remove = ArraySet[float, Dim[256]].new()
-    diff = abs(time() - spawn_time)
-
-    for key, entry in ParticleHandler.critical_flick_lane_effect.items():
-        if diff > 1 or entry.chunk_id == chunk_id or abs(time() - entry.spawn_time) > 1:
-            entry.particle.destroy()
-            keys_to_remove.add(key)
-
-    for key in keys_to_remove:
-        ParticleHandler.critical_flick_lane_effect.__delitem__(key)
-
-    min_i = floor(center_lane - size)
-    max_i = ceil(center_lane + size) - 1
-
-    if min_i < -127:
-        left_max_i = min(-128, max_i)
-
-        idx = -127.0
-        if idx in ParticleHandler.critical_flick_lane_effect:
-            ParticleHandler.critical_flick_lane_effect[idx].particle.destroy()
-
-        merged_center = (min_i + left_max_i + 1) / 2
-        merged_size = (left_max_i - min_i + 1) / 2
-
-        layout = layout_lane(merged_center, merged_size)
-        p = particles.lane.spawn(layout, duration=1 / Options.effect_animation_speed)
-        ParticleHandler.critical_flick_lane_effect[idx] = ParticleEntry(
-            particle=p, spawn_time=spawn_time, chunk_id=chunk_id
-        )
-
-    if max_i > 126:
-        right_min_i = max(127, min_i)
-
-        idx = 127.0
-        if idx in ParticleHandler.critical_flick_lane_effect:
-            ParticleHandler.critical_flick_lane_effect[idx].particle.destroy()
-
-        merged_center = (right_min_i + max_i + 1) / 2
-        merged_size = (max_i - right_min_i + 1) / 2
-
-        layout = layout_lane(merged_center, merged_size)
-        p = particles.lane.spawn(layout, duration=1 / Options.effect_animation_speed)
-        ParticleHandler.critical_flick_lane_effect[idx] = ParticleEntry(
-            particle=p, spawn_time=spawn_time, chunk_id=chunk_id
-        )
-
-    start_i = max(-127, min_i)
-    end_i = min(126, max_i)
-
-    for i in range(start_i, end_i + 1):
-        slot_lane = i + 0.5
-        if slot_lane in ParticleHandler.critical_flick_lane_effect:
-            ParticleHandler.critical_flick_lane_effect[slot_lane].particle.destroy()
-
-        layout = layout_lane(slot_lane, 0.5)
-        p = particles.lane.spawn(layout, duration=1 / Options.effect_animation_speed)
-
-        ParticleHandler.critical_flick_lane_effect[slot_lane] = ParticleEntry(
-            particle=p, spawn_time=spawn_time, chunk_id=chunk_id
-        )
