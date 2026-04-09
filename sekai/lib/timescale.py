659e81ba2a6f61b3174d137054124cb90acd2523
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-04-09 20:56:04 +0900

    feat: Use Neumeier summation on the timescale
    refactor: remove timescale caching
    
    refactor: implement Neumaier summation via __add__ operator

--- sekai/lib/timescale.py
+++ sekai/lib/timescale.py
@@ -24,6 +24,20 @@ class CompositeTime(Record):
     def total(self) -> float:
         return self.base + self.delta
 
+    def __add__(self, value: float) -> CompositeTime:
+        result = +CompositeTime
+        result.base = self.base
+        result.delta = self.delta
+
+        t = result.base + value
+        if abs(result.base) >= abs(value):
+            result.delta += (result.base - t) + value
+        else:
+            result.delta += (value - t) + result.base
+        result.base = t
+
+        return result
+
 
 class TimescaleChangeLike(Protocol):
     beat: float
@@ -62,7 +76,7 @@ class TimescaleGroupLike(Protocol):
 class TimeToScaledTime(Record):
     last_timescale: float
     last_time: float
-    last_scaled_time: float
+    last_scaled_time: CompositeTime
     last_ease: TimescaleEase
     first_change_index: int
     next_change_index: int
@@ -76,7 +90,8 @@ class TimeToScaledTime(Record):
     def reset(self):
         self.last_timescale = 1.0
         self.last_time = MIN_START_TIME
-        self.last_scaled_time = MIN_START_TIME
+        self.last_scaled_time.base = MIN_START_TIME
+        self.last_scaled_time.delta = 0.0
         self.last_ease = TimescaleEase.NONE
         self.next_change_index = self.first_change_index
         self.next_time = MIN_START_TIME
@@ -86,6 +101,7 @@ class TimeToScaledTime(Record):
         result = +CompositeTime
         if time <= MIN_START_TIME or Options.disable_timescale:
             result.base = time
+            result.delta = 0.0
             return result
         if time < self.last_time:
             self.reset()
@@ -110,13 +126,13 @@ class TimeToScaledTime(Record):
                     assert_never(self.last_ease)
         for change in iter_timescale_changes(self.next_change_index):
             next_timescale = change.timescale
-            next_time = change.cached_time
-            self.next_timescale = next_timescale
+            next_time = beat_to_time(change.beat)
+            next_scaled_time = +CompositeTime
             match self.last_ease:
                 case TimescaleEase.NONE:
-                    next_scaled_time = self.last_scaled_time + (next_time - self.last_time) * self.last_timescale
+                    next_scaled_time @= self.last_scaled_time + (next_time - self.last_time) * self.last_timescale
                 case TimescaleEase.LINEAR:
-                    next_scaled_time = (
+                    next_scaled_time @= (
                         self.last_scaled_time
                         + (next_time - self.last_time) * (next_timescale + self.last_timescale) / 2
                     )
@@ -124,37 +140,31 @@ class TimeToScaledTime(Record):
                     assert_never(self.last_ease)
             skip_scaled_time = change.cached_skip_time
             if time == next_time:
-                result.base = next_scaled_time + skip_scaled_time
+                result @= next_scaled_time + skip_scaled_time
                 return result
             if time < next_time:
                 if abs(next_time - self.last_time) < 1e-6:
-                    result.base = self.last_scaled_time
+                    result @= self.last_scaled_time
                     return result
                 match self.last_ease:
                     case TimescaleEase.NONE:
-                        result.base = self.last_scaled_time
-                        result.delta = (time - self.last_time) * self.last_timescale
+                        result @= self.last_scaled_time + (time - self.last_time) * self.last_timescale
                         return result
                     case TimescaleEase.LINEAR:
                         avg_timescale = (
                             self.last_timescale
                             + remap(self.last_time, next_time, self.last_timescale, next_timescale, time)
                         ) / 2
-                        result.base = self.last_scaled_time
-                        result.delta = (time - self.last_time) * avg_timescale
+                        result @= self.last_scaled_time + (time - self.last_time) * avg_timescale
                         return result
                     case _:
                         assert_never(self.last_ease)
             self.last_timescale = next_timescale
             self.last_time = next_time
-            self.last_scaled_time = next_scaled_time + skip_scaled_time
+            self.last_scaled_time @= next_scaled_time + skip_scaled_time
             self.last_ease = change.timescale_ease
             self.next_change_index = change.next_ref.index
-        self.next_time = inf
-        self.next_timescale = self.last_timescale
-
-        result.base = self.last_scaled_time
-        result.delta = (time - self.last_time) * self.last_timescale
+        result @= self.last_scaled_time + (time - self.last_time) * self.last_timescale
         return result
 
 
@@ -195,7 +205,7 @@ class TimeToLastChangeIndex(Record):
 class ScaledTimeToFirstTime(Record):
     last_timescale: float
     last_time: float
-    last_scaled_time: float
+    last_scaled_time: CompositeTime
     last_ease: TimescaleEase
     first_change_index: int
     next_change_index: int
@@ -208,7 +218,8 @@ class ScaledTimeToFirstTime(Record):
     def reset(self):
         self.last_timescale = 1.0
         self.last_time = MIN_START_TIME
-        self.last_scaled_time = MIN_START_TIME
+        self.last_scaled_time.base = MIN_START_TIME
+        self.last_scaled_time.delta = 0.0
         self.last_ease = TimescaleEase.NONE
         self.next_change_index = self.first_change_index
         self.last_query_scaled_time = MIN_START_TIME
@@ -221,30 +232,35 @@ class ScaledTimeToFirstTime(Record):
         self.last_query_scaled_time = scaled_time
         for change in iter_timescale_changes(self.next_change_index):
             next_timescale = change.timescale
-            next_time = change.cached_time
+            next_time = beat_to_time(change.beat)
+            next_scaled_time = +CompositeTime
             match self.last_ease:
                 case TimescaleEase.NONE:
-                    next_scaled_time = self.last_scaled_time + (next_time - self.last_time) * self.last_timescale
-                    if (self.last_scaled_time <= scaled_time <= next_scaled_time and self.last_timescale > 0) or (
-                        self.last_scaled_time >= scaled_time >= next_scaled_time and self.last_timescale < 0
+                    next_scaled_time @= self.last_scaled_time + (next_time - self.last_time) * self.last_timescale
+                    lst_tot = self.last_scaled_time.total
+                    nst_tot = next_scaled_time.total
+                    if (lst_tot <= scaled_time <= nst_tot and self.last_timescale > 0) or (
+                        lst_tot >= scaled_time >= nst_tot and self.last_timescale < 0
                     ):
-                        if abs(next_scaled_time - self.last_scaled_time) < 1e-6:
+                        if abs(nst_tot - lst_tot) < 1e-6:
                             return self.last_time
-                        return remap(self.last_scaled_time, next_scaled_time, self.last_time, next_time, scaled_time)
+                        return remap(lst_tot, nst_tot, self.last_time, next_time, scaled_time)
                 case TimescaleEase.LINEAR:
-                    next_scaled_time = (
+                    next_scaled_time @= (
                         self.last_scaled_time
                         + (next_time - self.last_time) * (next_timescale + self.last_timescale) / 2
                     )
+                    lst_tot = self.last_scaled_time.total
+                    nst_tot = next_scaled_time.total
                     if abs(next_time - self.last_time) < 1e-6:
-                        lo_scaled_time = min(self.last_scaled_time, next_scaled_time)
-                        hi_scaled_time = max(self.last_scaled_time, next_scaled_time)
+                        lo_scaled_time = min(lst_tot, nst_tot)
+                        hi_scaled_time = max(lst_tot, nst_tot)
                         if lo_scaled_time <= scaled_time <= hi_scaled_time:
                             return self.last_time
                     else:
                         a = (next_timescale - self.last_timescale) / (next_time - self.last_time)
                         b = self.last_timescale
-                        c = self.last_scaled_time - scaled_time
+                        c = lst_tot - scaled_time
 
                         first_time = inf
                         found_time = False
@@ -269,18 +285,19 @@ class ScaledTimeToFirstTime(Record):
                 case _:
                     assert_never(self.last_ease)
             skip_scaled_time = change.timescale_skip * 60 / beat_to_bpm(change.beat)
-            if (next_scaled_time <= scaled_time <= next_scaled_time + skip_scaled_time) or (
-                next_scaled_time + skip_scaled_time <= scaled_time <= next_scaled_time
+            nst_tot = next_scaled_time.total
+            if (nst_tot <= scaled_time <= nst_tot + skip_scaled_time) or (
+                nst_tot + skip_scaled_time <= scaled_time <= nst_tot
             ):
                 return next_time
             self.last_timescale = next_timescale
             self.last_time = next_time
-            self.last_scaled_time = next_scaled_time + skip_scaled_time
+            self.last_scaled_time @= next_scaled_time + skip_scaled_time
             self.last_ease = change.timescale_ease
             self.next_change_index = change.next_ref.index
         if self.last_timescale == 0:
             return inf
-        additional_time = (scaled_time - self.last_scaled_time) / self.last_timescale
+        additional_time = (scaled_time - self.last_scaled_time.total) / self.last_timescale
         if additional_time < 0:
             return inf
         return self.last_time + additional_time
@@ -356,7 +373,8 @@ def group_time_to_scaled_time(
 ) -> CompositeTime:
     if isinstance(group, EntityRef):
         group = group.index
-    return timescale_group_archetype().at(group).time_to_scaled_time.get(time)
+    group_entity = timescale_group_archetype().at(group)
+    return group_entity.time_to_scaled_time.get(time)
 
 
 def group_scaled_time_to_first_time(
@@ -365,7 +383,8 @@ def group_scaled_time_to_first_time(
 ) -> float:
     if isinstance(group, EntityRef):
         group = group.index
-    return timescale_group_archetype().at(group).scaled_time_to_first_time.get(scaled_time)
+    group_entity = timescale_group_archetype().at(group)
+    return group_entity.scaled_time_to_first_time.get(scaled_time)
 
 
 def group_scaled_time_to_first_time_2(
@@ -375,7 +394,8 @@ def group_scaled_time_to_first_time_2(
     # This is a second function so that we don't have to reset the timescale groups as often
     if isinstance(group, EntityRef):
         group = group.index
-    return timescale_group_archetype().at(group).scaled_time_to_first_time_2.get(scaled_time)
+    group_entity = timescale_group_archetype().at(group)
+    return group_entity.scaled_time_to_first_time_2.get(scaled_time)
 
 
 def update_timescale_group(group: int | EntityRef) -> None:
