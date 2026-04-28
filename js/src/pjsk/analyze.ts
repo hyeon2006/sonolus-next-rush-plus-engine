79e933f62e8bed296247750e9e9c3d5627cc0f9a
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-04-28 12:17:11 +0900

    Remove Sekai Official Assets

--- js/src/pjsk/analyze.ts
+++
@@ -1,9 +0,0 @@
-/** Check if it is PJSK */
-export const isPJSK = (data: unknown): boolean => {
-    if (typeof data !== 'object' || data === null) return false
-
-    const hasNotes = 'notes' in data && Array.isArray((data as Record<string, unknown>).objects)
-    const hasVersionOrOffset = 'version' in data || 'offset' in data
-
-    return hasNotes && hasVersionOrOffset
-}
